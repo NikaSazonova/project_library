e5412221200475a931d80225457b14f6cc207ecb
Varvara <miss.v-ermolina@yandex.ru>
2021-04-16 17:07:39 +0300

    #code for designs

---
+++ main.py
@@ -0,0 +1,156 @@
+import datetime
+
+from flask import Flask, request, abort, session, make_response, render_template, redirect
+from data import db_session
+from data.users import User
+from data.books import Books
+from forms.user import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from forms.book import BookForm
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/library.db")
+    app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    books = db_sess.query(Books)
+    return render_template("index.html", books=books)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/book', methods=['GET', 'POST'])
+@login_required
+def add_book():
+    form = BookForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        book = Books()
+        book.title = form.title.data
+        book.pic_url = form.pic_url.data
+        book.author = form.author.data
+        book.content = form.content.data
+        current_user.books.append(book)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('books.html', title='Добавление новости',
+                           form=form)
+
+
+@app.route('/book/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(id):
+    form = BookForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        book = db_sess.query(Books).filter(Books.id == id,
+                                           Books.user == current_user
+                                           ).first()
+        if book:
+            form.title.data = book.title
+            form.author.data = book.author
+            form.pic_url.data = book.pic_url
+            form.content.data = book.content
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        book = db_sess.query(Books).filter(Books.id == id,
+                                           Books.user == current_user
+                                           ).first()
+        if book:
+            book.title = form.title.data
+            book.author = form.author.data
+            book.pic_url = form.pic_url.data
+            book.content = form.content.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('books.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
+
+
+@app.route('/book_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def book_delete(id):
+    db_sess = db_session.create_session()
+    book = db_sess.query(Books).filter(Books.id == id,
+                                       Books.user == current_user
+                                       ).first()
+    if book:
+        db_sess.delete(book)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    main()
