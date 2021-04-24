53c429d11bb475263f7578d9cbf87b8378fafd55
NikaSazonova <74972433+NikaSazonova@users.noreply.github.com>
2021-04-24 22:47:56 +0300

    Update search.py

--- data/search.py
+++ data/search.py
@@ -2,8 +2,4 @@ import sqlite3
 
 
 def search(request):
-    connec = sqlite3.connect("project_library/bd/library.db")
-    curs = connec.cursor()
-    sor_t = curs.execute("""SELECT * FROM books
-                WHERE title LIKE '?'""", (f'%{request}%',)).fetchall()
-    connec.close()
+    query(books).filter(books.title.like(f'%{request}%'))
